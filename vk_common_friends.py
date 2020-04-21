4797e82c5e35c2ee3bae58f8b5f8f82ae0361f4a
Ivan Alexandrov <radniforg@yandex.ru>
2020-04-21 14:41:52 +0300

    & version with personal data

--- vk_common_friends.py
+++ vk_common_friends.py
@@ -15,34 +15,41 @@ class User:
     def __str__(self):
         return self.user_url
 
+    def __and__(self, other_user):
+        response_first = requests.get(
+            'https://api.vk.com/method/friends.get',
+            params={
+                'access_token': TOKEN,
+                'user_id': self.user_id,
+                'v': 5.103
+            }
+        )
+        response_second = requests.get(
+            'https://api.vk.com/method/friends.get',
+            params = {
+                'access_token': TOKEN,
+                'user_id': other_user.user_id,
+                'v': 5.103
+            }
+        )
+        ids = ''
+        for first_user_friend in response_first.json()['response']['items']:
+            for second_user_friend in response_second.json()['response']['items']:
+                if first_user_friend == second_user_friend:
+                    ids = ids + ', ' + str(first_user_friend)
+        user_response = requests.get(
+                'https://api.vk.com/method/users.get',
+                params={
+                    'access_token': TOKEN,
+                    'user_ids': ids,
+                    'v': 5.103
+                }
+            )
+        mutual_friends_list = []
+        for friends in user_response.json()['response']:
+            mutual_friends_list.append(User(friends['id'], friends['first_name'], friends['last_name']))
+        return mutual_friends_list
 
-def mutual_friends(token, user1_id, user2_id):
-    response = requests.get(
-        'https://api.vk.com/method/friends.getMutual',
-        params={
-            'access_token': token,
-            'source_uid': user1_id,
-            'target_uid': user2_id,
-            'fields': 'city',
-            'v': 5.103
-        }
-    )
-    temp_dict = response.json()
-    ids = ''
-    for new_id in temp_dict['response']:
-        ids = ids + ', ' + str(new_id)
-    user_response = requests.get(
-        'https://api.vk.com/method/users.get',
-        params={
-            'access_token': TOKEN,
-            'user_ids': ids,
-            'v': 5.103
-        }
-    )
-    mutual_friends_list = []
-    for friends in user_response.json()['response']:
-        mutual_friends_list.append(User(friends['id'], friends['first_name'], friends['last_name']))
-    return mutual_friends_list
 
 APP_ID = 7412577
 OAUTH_URL = 'https://oauth.vk.com/authorize'
@@ -61,7 +68,7 @@ user1 = User(4243253, 'EL', 'AL')
 user2 = User(23289398, 'AN', 'SE')
 
 # строка ниже использовалась для проверки работоспособности кода
-print(mutual_friends(TOKEN, user1.user_id, user2.user_id)[0])
+pprint(user1&user2)
 
 
 
